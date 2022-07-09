bb7528449b233bf32b0a62bcb11030b6a03f79d3
natalia.yelistratova <natalia.yelistratova@gmail.com>
2022-07-09 17:48:11 +0300

    Create and call getUserName function.

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+export function getUserName(){
+  const name = readlineSync.question('May I have your name: ');
+  console.log(`Hello, ${name}!`);
+  return;
+}
+
+
