17b3947ec69b6cee2e4571705240264b80b66c56
natalia.yelistratova <natalia.yelistratova@gmail.com>
2022-07-16 17:42:19 +0300

    Refactor even game code.

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,23 +3,34 @@ import {
   roundsQuantity, welcomeUser, getUserName, getRandomNumber,
 } from '../index.js';
 
+function isNumberEven(number) {
+  return number % 2 === 0;
+}
+
+function showRules() {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+}
+
+function isUserAnswerCorrect(userAnswer, correctAnswer) {
+  return userAnswer.toLowerCase() === correctAnswer;
+}
+
 export default function brainEvenGame() {
   welcomeUser();
-
   const userName = getUserName();
-
-  function isNumberEven(number) {
-    return number % 2 === 0;
-  }
-
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  showRules();
 
   for (let i = 0; i < roundsQuantity; i++) {
     const randomNumber = getRandomNumber();
-    console.log(`Question: ${randomNumber}`);
     const correctAnswer = isNumberEven(randomNumber) ? 'yes' : 'no';
+
+    console.log(`Question: ${randomNumber}`);
+
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer.toLowerCase() === correctAnswer) {
+
+    const result = isUserAnswerCorrect(userAnswer, correctAnswer);
+
+    if (result) {
       console.log('Correct!');
       if (i === roundsQuantity - 1) {
         console.log(`Congratulations, ${userName}!`);
@@ -27,7 +38,7 @@ export default function brainEvenGame() {
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let\'s try again, ${userName}!`);
-      break;
+      return;
     }
   }
 }
