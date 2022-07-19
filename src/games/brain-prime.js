24e3f743addbc3451a76036e7eb982f272662b64
natalia.yelistratova <natalia.yelistratova@gmail.com>
2022-07-19 16:36:03 +0300

    Prime game logic.

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -6,11 +6,22 @@ import {
 } from '../index.js';
 
 function calcResult(value) {
-  return true;
+  if (value === 1) {
+    return false;
+  } else if (value === 2) {
+    return true;
+  } else {
+    for (let i = 2; i < value; i++) {
+      if (value % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 function checkAnswer(userAnswer, correctAnswer) {
-  return Number(userAnswer) === correctAnswer;
+  return userAnswer.toLowerCase() === correctAnswer;
 }
 
 export default function brainPrimeGame() {
@@ -19,7 +30,7 @@ export default function brainPrimeGame() {
 
   for (let i = 0; i < roundsQuantity; i++) {
     const number = getRandomNumber();
-    const correctAnswer = calcResult(number);
+    const correctAnswer = calcResult(number) ? 'yes' : 'no';
     const result = playRound(i, number, correctAnswer, checkAnswer);
 
     if (!result) {
