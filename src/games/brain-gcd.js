17719d73598bdde1a160fcb4e9ffde920727d160
natalia.yelistratova <natalia.yelistratova@gmail.com>
2022-07-19 15:38:22 +0300

    Calc result for gcd.

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -5,8 +5,24 @@ import {
   playRound,
 } from '../index.js';
 
-// function calcResult(value1, value2) {
-// }
+function calcResult(value1, value2) {
+  // use Euclid's method
+  let dividend = Math.max(value1, value2);
+  let divisor = Math.min(value1, value2);
+  let reminder = dividend % divisor;
+  let result;
+
+  while (reminder > 0) {
+    reminder = dividend % divisor;
+    if (reminder === 0) {
+      result = divisor;
+    }
+    dividend = divisor;
+    divisor = reminder;
+  }
+
+  return result;
+}
 
 function checkAnswer(userAnswer, correctAnswer) {
   return Number(userAnswer) === correctAnswer;
@@ -19,7 +35,7 @@ export default function brainGcdGame() {
   for (let i = 0; i < roundsQuantity; i++) {
     const number1 = getRandomNumber();
     const number2 = getRandomNumber();
-    // const correctAnswer = calcResult(number1, number2);
+    const correctAnswer = calcResult(number1, number2);
     const questionValue = `${number1} ${number2}`;
     const result = playRound(i, questionValue, correctAnswer, checkAnswer);
 
