2dd8d9afc24aea7e7264593f034eb9b2416cfd14
natalia.yelistratova <natalia.yelistratova@gmail.com>
2022-07-27 13:22:29 +0300

    Rewrite brain-gcd game.

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -3,39 +3,30 @@ import {
   playGame,
 } from '../index.js';
 
-let number1;
-let number2;
-
-function calcResult(value1, value2) {
+function calcGcd(value1, value2) {
   const reminder = value1 % value2;
 
   if (reminder === 0) {
     return value2;
   } else {
-    return calcResult(value2, reminder);
+    return calcGcd(value2, reminder);
   }
 }
 
-function createQuestionValue() {
-  number1 = getRandomNumber();
-  number2 = getRandomNumber();
+function getRoundData() {
+  const number1 = getRandomNumber();
+  const number2 = getRandomNumber();
+  const questionValue = `${number1} ${number2}`;
 
-  return `${number1} ${number2}`;
-}
-
-function calcCorrectAnswer() {
   const graterNumber = Math.max(number1, number2);
   const lowerNumber = Math.min(number1, number2);
+  const correctAnswer = calcGcd(graterNumber, lowerNumber);
 
-  return calcResult(graterNumber, lowerNumber);
-}
-
-function checkAnswer(userAnswer, correctAnswer) {
-  return Number(userAnswer) === correctAnswer;
+  return { questionValue, correctAnswer };
 }
 
 export default function brainProgression() {
   const rules = 'Find the greatest common divisor of given numbers.';
 
-  playGame(rules, createQuestionValue, calcCorrectAnswer, checkAnswer);
+  playGame(rules, getRoundData);
 }
