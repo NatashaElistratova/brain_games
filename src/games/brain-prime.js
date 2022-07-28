a3e485681748298a19d1e7ba01d5c4129e2a4143
natalia.yelistratova <natalia.yelistratova@gmail.com>
2022-07-28 18:08:03 +0300

    Fix linter issue.

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -22,7 +22,7 @@ const getRoundData = () => {
   const correctAnswer = isPrime(questionValue) ? 'yes' : 'no';
 
   return { questionValue, correctAnswer };
-}
+};
 
 export default function runBrainPrimeGame() {
   const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
